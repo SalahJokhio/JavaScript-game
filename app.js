9ed9ccbfdca43c2e6940790fb575aae6bb332bb8
SalahJokhio <154239945+SalahJokhio@users.noreply.github.com>
2024-04-28 13:12:08 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -21,54 +21,87 @@ var dice_input = document.getElementById("dice_input");
 //   }
 // }
 
+function focusInput() {
+  document.getElementById('input_dice').focus();
+}
 
-        var dice = document.getElementById("dice");
-        var win_loss = document.getElementById("win_loss");
-        var input_dice = document.getElementById("input_dice");
-        var turnsLeftDisplay = document.getElementById("turns_left");
-        var correctAttemptsDisplay = document.getElementById("correct_attempts");
-        var wrongAttemptsDisplay = document.getElementById("wrong_attempts");
+document.addEventListener('DOMContentLoaded', function() {
+  focusInput();
+});
 
-        var attempts = 0;
-        var correctAnswers = 0;
-        var turnsLeft = 10;
+var dice = document.getElementById("dice");
+var win_loss = document.getElementById("win_loss");
+var input_dice = document.getElementById("input_dice");
+var turnsLeftDisplay = document.getElementById("turns_left");
+var correctAttemptsDisplay = document.getElementById("correct_attempts");
+var wrongAttemptsDisplay = document.getElementById("wrong_attempts");
 
-        function updateUI() {
-            turnsLeftDisplay.innerText = "Turns left: " + turnsLeft;
-            correctAttemptsDisplay.innerText = "Correct attempts: " + correctAnswers;
-            wrongAttemptsDisplay.innerText = "Wrong attempts: " + (attempts - correctAnswers);
-        }
+var attempts = 0;
+var correctAnswers = 0;
+var turnsLeft = 10;
+var congratulationShown = false; 
 
-        function gendicenumber() {
-            if (attempts < 10 && correctAnswers < 3) {
-                var khiladiValue = input_dice.value;
-                if (khiladiValue >= 1 && khiladiValue <= 6) {
-                    var dicenumber = Math.ceil(Math.random() * 6);
-                    dice.innerText = dicenumber;
-                    attempts++;
-                    turnsLeft--;
-                    updateUI();
-                    if (khiladiValue == dicenumber) {
-                        correctAnswers++;
-                        win_loss.innerText = "You win";
-                        win_loss.className = "firework";
-                        win_loss.style.color = "Green";
-                    } else {
-                        win_loss.innerText = "Bad Luck";
-                        win_loss.style.color = "orange";
-                    }
-                } else {
-                    win_loss.innerText = "Please enter a number between 1 and 6";
-                    win_loss.style.color = "red";
-                }
-            }
-            
-            if (correctAnswers >= 3) {
-                win_loss.innerText = "Congratulations! You've won!";
-                win_loss.style.color = "Green";
-            } else if (attempts >= 10) {
-                win_loss.innerText = "Sorry, you've lost!";
-                win_loss.style.color = "Red";
-            }
-            input_dice.value = "";
-        }
+function updateUI() {
+turnsLeftDisplay.innerText = "Turns left: " + turnsLeft;
+correctAttemptsDisplay.innerText = "Correct attempts: " + correctAnswers;
+wrongAttemptsDisplay.innerText =
+  "Wrong attempts: " + (attempts - correctAnswers);
+}
+
+function gendicenumber() {
+if (attempts < 10 && correctAnswers < 3) {
+  var khiladiValue = input_dice.value;
+  if (khiladiValue >= 1 && khiladiValue <= 6) {
+    var dicenumber = Math.ceil(Math.random() * 6);
+    dice.innerText = dicenumber;
+    attempts++;
+    turnsLeft--;
+    updateUI();
+    if (khiladiValue == dicenumber) {
+      correctAnswers++;
+      win_loss.innerText = "You win";
+      win_loss.className = "firework";
+      win_loss.style.color = "Green";
+      if (correctAnswers === 1 && !congratulationShown) {
+        showCongratulation(); 
+      }
+    } else {
+      win_loss.innerText = "Bad Luck";
+      win_loss.style.color = "orange";
+    }
+  } else {
+    win_loss.innerText = "Please enter a number between 1 and 6";
+    win_loss.style.color = "red";
+  }
+}
+
+if (correctAnswers >= 3) {
+  win_loss.innerText = "Congratulations! You've won!";
+  win_loss.style.color = "Green";
+} else if (attempts >= 10) {
+  win_loss.innerText = "Sorry, you've lost!";
+  win_loss.style.color = "Red";
+}
+input_dice.value = "";
+focusInput(); 
+}
+
+function showCongratulation() {
+
+win_loss.innerText = "Congratulations!";
+win_loss.className = "firework"; 
+win_loss.style.color = "Green";
+congratulationShown = true;
+setTimeout(function() {
+  win_loss.innerText = ""; 
+}, 2000); 
+}
+
+function giveReward() {
+
+var rewardMessage = document.createElement("p");
+rewardMessage.innerText = "Congratulations! Here's your reward: 🎁";
+rewardMessage.style.fontSize = "20px";
+rewardMessage.style.color = "blue";
+document.getElementById("game").appendChild(rewardMessage);
+}
